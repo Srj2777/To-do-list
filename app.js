452d4e95b7c73174071df64368f8d232781e2d15
Suraj Jaiswal <90308007+Srj2777@users.noreply.github.com>
2023-04-07 19:02:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
 
 const app = express();
 
